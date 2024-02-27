d44516a717cb4dcddac47e5e24034ff995607a6d
MilewskiMaciej <milewskimaciej@o2.pl>
2024-02-27 16:09:11 +0100

    commited

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import sys
 
-liczba = input('Wprowadz liczbe: ')
-print(liczba)
+#liczba = input('Wprowadz liczbe: ')
+#print(liczba)
 
-sys.stdout.write('Wprowadz liczbe: ')
-liczba = sys.stdin.readline()
-liczba = int(liczba)
-print(liczba + 'to wczytana liczba')
+#sys.stdout.write('Wprowadz liczbe: ')
+#liczba = sys.stdin.readline()
+#print(type(liczba))
+#liczba = int(liczba)
+#print(liczba)
+
+a = int(input())
+b = int(input())
+if a > b:
+    print(a)
+else:
+    print(b)
+if a == b:
+    print(a+b)
+else:
+    print('liczby sa nierowne')
 
 print("Kys Mr Milewski")
