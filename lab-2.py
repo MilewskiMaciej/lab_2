02b89d69687538f92ab310933827c57bd05bdf24
MilewskiMaciej <milewskimaciej@o2.pl>
2024-02-27 17:25:03 +0100

    pr_dom_1

---
+++ lab-2.py
@@ -0,0 +1,46 @@
+import sys
+
+#def zad1():
+#    fraza = input('Wprowadz wyrazenie: ')
+#    licznik = 0
+#    licz = fraza.split()
+#    for i in licz:
+#        licznik += 1
+#    print('Ilosc slow:', licznik)
+
+#def zad2():
+#    sys.stdout.write('Wprowadz pierwsza liczbe: ')
+#    pierwsza = sys.stdin.readline()
+#    pierwsza = int(pierwsza)
+#    sys.stdout.write('Wprowadz druga liczbe: ')
+#    druga = sys.stdin.readline()
+#    sys.stdout.write('Wprowadz trzecia liczbe: ')
+#    trzecia = sys.stdin.readline()
+#    trzecia = int(trzecia)
+#    print((pierwsza**druga)+trzecia)
+
+#def zad3():
+#    slowo = input('Wprowadz slowo: ')
+#    pal = list(slowo)
+#    sprawdz = pal[::-1]
+#    if pal == sprawdz:
+#        print('Podane slowo jest palindromem')
+#    else:
+#        print('Podane slowo nie jest palindromem')
+
+#def zad7():
+#    dz_liczby = []
+#    licznik = 0
+#    while licznik < 10:
+#        dz_licz = int(input('Podaj liczbe: '))
+#        if dz_licz % 2 == 0:
+#            dz_liczby.append(dz_licz)
+#        licznik += 1
+#    print(dz_liczby)
+
+def main():
+#    zad1()
+#    zad2()
+#    zad3()
+#    zad7()
+main()
