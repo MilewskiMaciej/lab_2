35564721ae25f111ad1bc9693e667c673a60accb
MilewskiMaciej <milewskimaciej@o2.pl>
2024-02-27 16:25:04 +0100

    commited

--- main.py
+++ main.py
@@ -9,15 +9,35 @@ import sys
 #liczba = int(liczba)
 #print(liczba)
 
-a = int(input())
-b = int(input())
-if a > b:
-    print(a)
-else:
-    print(b)
-if a == b:
-    print(a+b)
-else:
-    print('liczby sa nierowne')
+#a = int(input())
+#b = int(input())
+#if a > b:
+#    print(a)
+#else:
+#    print(b)
+#if a == b:
+#    print(a+b)
+#else:
+#    print('liczby sa nierowne')
 
-print("Kys Mr Milewski")
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#d = int(input())
+#if (a > b) & (c > d):
+#    print(a+c)
+#else:
+#    print(b+d)
+
+#for i in range(0,6):
+#    print(i)
+
+#licznik = 0
+#while licznik < 10:
+#    print(licznik)
+#    licznik += 1
+
+lista = [1,2,3,4,5,7,2,3]
+x = int(input('Podaj liczbe: '))
+if (x**x in lista):
+    print('Znalezioned')
